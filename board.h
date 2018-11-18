0a718730b60f234be67308d6917804d5046d9828
Kevin Zhang <kevin.zg98@gmail.com>
2018-11-17 21:58:18 -0500

    Add files via upload
    initial commit

---
+++ board.h
@@ -0,0 +1,27 @@
+#ifndef __BOARD__H
+#define __BOARD__H
+
+
+#include "cell.h"
+#include "player.h"
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+class Board{
+private:
+	Player p1;
+	Player p2;
+public:
+	Board();
+	friend std::ostream& operator<<(std::ostream &out, Board &b);
+	void printBoards(); // prints all the board stuff for the game
+	void Move(int,std::string) // (integer, string)
+};
+
+
+
+std::ostream& operator<<(std::ostream &out, Board &b);
+
+
+#endif
