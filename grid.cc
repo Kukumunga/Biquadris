dfe6281acba24e301146424e7705024418a2e1a8
Kevin Zhang <kevin.zg98@gmail.com>
2018-11-17 23:20:50 -0500

    Add files via upload

---
+++ grid.cc
@@ -0,0 +1,27 @@
+#include "grid.h"
+
+Grid::Grid(){
+
+	for (int x = 0; x < 11;++x){ // columns
+		std::vector<Cell> vec;
+		for (int y = 0; y < 18;++y){ // row
+			Cell c = Cell();
+			vec.emplace_back(c);
+		}
+		theGrid.emplace_back(vec);
+	}
+
+	
+	
+}
+
+void Grid::printRow(int r){
+	for (int x = 0; x < 11; ++ x){
+		std::cout << theGrid[x][r];
+	}
+}
+
+Grid::addToCurrent(){
+	blocksInGrid.emplace_back(*currentBlock)
+
+}
