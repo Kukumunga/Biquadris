d9d932347a6c2e7269a25b8fcefb91b4af320140
jmficker <jf.97.baseball@hotmail.com>
2018-11-24 17:05:10 -0500

    Update blocko.cc

--- blocko.cc
+++ blocko.cc
@@ -1,6 +1,6 @@
 #include "blocko.h"
 using namespace std;
 
-BlockO::BlockO:Block(),type{"o"},colour{"blue"}{}
+BlockO::BlockO():Block(),type{"o"},colour{"blue"}{}
 
 string BlockO::getType(){return type;}
