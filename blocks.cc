86b734b844bcc15de35e64ecc2e9d139a7a03e63
jmficker <jf.97.baseball@hotmail.com>
2018-11-24 17:05:27 -0500

    Update blocks.cc

--- blocks.cc
+++ blocks.cc
@@ -1,6 +1,6 @@
 #include "blocks.h"
 using namespace std;
 
-BlockS::BlockS:Block(),type{"s"},colour{"yellow"}{}
+BlockS::BlockS():Block(),type{"s"},colour{"yellow"}{}
 
 string BlockS::getType(){return type;}
