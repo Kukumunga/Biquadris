0752c7156638e7481296646cc4db1d4b3a513d1c
jmficker <jf.97.baseball@hotmail.com>
2018-11-24 17:03:02 -0500

    Update block.cc

--- block.cc
+++ block.cc
@@ -9,6 +9,7 @@ void Block::setLevel(int l){level = l;}
 
 void Block::addCell(Cell *cell){components.emplace_back(cell);}
 
+/*
 vector<Coord *> Block::calcLeft(){
 	vector<Coord *> newCoords;
 	for (int comp = 0; comp < 4; comp++){
@@ -16,7 +17,7 @@ vector<Coord *> Block::calcLeft(){
 	}
 	
 }	
-
+*/
 //vector<Coord *> Block::rotateClock(){
 //	beforeCoord = 
 //}
