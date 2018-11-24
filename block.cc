bc1ba4dd4e040d83233edb792e2ef451b533e358
jmficker <jmficker@student.cs.uwaterloo.ca>
2018-11-24 16:31:09 -0500

    commit

--- block.cc
+++ block.cc
@@ -9,7 +9,13 @@ Block::setLevel(int l){level = l;}
 
 Block::addCell(Cell *cell){components.emplace_back(cell);}
 
-
+vector<Coord *> Block::calcLeft(){
+	vector<Coord *> newCoords;
+	for (int comp = 0; comp < 4; comp++){
+		beforeCoords.emplace_back(component[comp].getCoord()); //get old Coords
+	}
+	
+}	
 
 //vector<Coord *> Block::rotateClock(){
 //	beforeCoord = 
