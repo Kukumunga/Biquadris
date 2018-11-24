57b644622d9b05b676f9830bebb778227d6cdf30
jmficker <jmficker@student.cs.uwaterloo.ca>
2018-11-24 15:18:11 -0500

    made changes

--- block.h
+++ block.h
@@ -1,10 +1,29 @@
 #ifndef __BLOCK__
 #define __BLOCK__
 
+class Cell;
+class Coord;
 class Block {
-
-
-
+	std::vector<Cell *> components; 
+	std::string type;
+	std::string colour;
+	int level; //level the block was created
+	std::string orientation; //the block's orientation
+public:
+	Block();
+	virtual std::string getType();
+	void addCell(Cell*);
+	void setLevel(int l);
+	//virtual std::vector<Coord*> rotateClock();
+	//virtual std::vector<Coord*> rotateCounterClock();
+	//std::vector<Coord*> calcLeft();
+	//std::vector<Coord*> calcRight();
+	//std::vector<Coord*> calcDown();
+	//void executeLeft();
+	//void executeRight();
+	//void executeDown();
+	//virtual void executeClock();
+	//virtual void executeCounterClock();
 };
 
 #endif
