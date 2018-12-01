682cf0d54f1b64112d9d522414f0665f175336c2
akukuruz <akukuruz@student.cs.uwaterloo.ca>
2018-12-01 14:05:42 -0500

    new interpreter

--- interpreter.cc
+++ interpreter.cc
@@ -1,3 +1,38 @@
 #include "interpreter.h"
 
+Interpreter::Interpreter(){}
+std::vector<std::string> Interpreter::getCommands(std::string c){
+	std::cout << "fuck me" << std::endl;
+	commands.clear();
+	std::string number;
+	int n;
+	//fix invalid commands
+	int size = c.size();
+	std::string command;
+	for(int i = 0; i < size; ++i) {
+		std::string test;
+		test.push_back(c[i]);
+    	std::istringstream ss(test);
+    	if(ss >> n){
+    		number+=test;
+    	}
+    	else {
+    		if(number.size() > 0){
+    			std::istringstream ss(number);
+    			ss >> n;
+				command = c.substr(i);
+    		}
+    		else {
+    			command = c;
+    			n = 1;
+    		}    		
+    		break;
+    	}
+	}
+	for(int i = 0; i < n; ++i){
+		commands.emplace_back(command);
+	}
+	return commands;
+}
+
 Interpreter::~Interpreter(){}
--- interpreter.h
+++ interpreter.h
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
+#include <map>
+#include <sstream>
 
 class Interpreter {
+private:
+	std::vector<std::string> commands;
+    std::map<std::string, std::string> moves;
+	void multiplier(std::string command, int n);
+	void autocomplete();
   public:
-    virtual std::vector<std::string> getCommands(std::string c) = 0;
-    virtual ~Interpreter();
+  	Interpreter();
+    std::vector<std::string> getCommands(std::string c);
+    ~Interpreter();
 };
 
 #endif
