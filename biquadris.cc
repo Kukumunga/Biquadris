55dc97d7dea8ef4aed75838a9652cdb36c8d2e98
akukuruz <akukuruz@student.cs.uwaterloo.ca>
2018-12-02 13:25:49 -0500

    documentation

--- biquadris.cc
+++ biquadris.cc
@@ -8,28 +8,52 @@ Biquadris::~Biquadris(){}
 void Biquadris::start(){
 try{
 	std::cout << *gameBoard << std::endl;
+	// variable that gets the commands from the stream
 	std::string action;	
+	// stream source for sequence files
 	std::ifstream f;  
+	// binary varaible to tell if reading from terminal or file
+	// 0 is terminal
+	// 1 is file
 	bool stream = 0;  
+	// the last action to be called
+	std::string lastaction;
 	while(true){
+		// to hold the vector of moves returned from the command interpreter
 		std::vector<std::string> moves;			
+		// if we reach the end a sequence file, 
+		// we return the stream source back to the terminal
+		// and let the player who called the sequence to keep playing if the last action was not drop
+		// otherwise go to endgame
 		if (source->eof()){
 			if(stream == 1){
-				player = (player + 1)%2;
 				stream = 0;
 				source = &std::cin;
+				if(lastaction != "drop"){
+					// need to fix in case the play an even number of drops
+					player = (player + 1)%2;
+				}
 			}
 			else {
 				goto endgame;
 			}
 		}
+		// main loop for command interpretation
 		while(*source >> action){
 			if (action == "exit"){
 				goto endgame;
 			}
+			// moves will hold a vector of commands 
+			// where the size of the vector is the number of times
+			// the command is done (if command is able to be repeated)
 			moves = inter.getCommands(action);
+			// move is the command that will be executed 
 			std::string move = moves[0];
+			// set lastaction to the current command
+			lastaction = move;
+			// number of times the command will be exectued 
 			int size = moves.size();
+			// reads in a file, which becomes the new stream source
 			if (move == "sequence"){
 				std::string file;
 				*source >> file;
@@ -42,20 +66,25 @@ try{
 					std::cout << "Not a valid file" << std::endl;
 				}
 			}
+			// reverts the board back to random
 			else if (move == "random"){
 				gameBoard->random(player);
 			}
+			// reads in a file and calls noRandom on the board
 			else if (move == "norandom"){
 				std::string file;
 				*source >> file;
 				gameBoard->noRandom(player,file);
 			}
+			// restarts the game and keeps track of the highscore
 			else if (move == "restart"){
 				gameBoard->Restart();
 				player = 0;
 				std::cout << *gameBoard << std::endl;
 				break;
 			}
+			// where most board actions are done, such as moving 
+			// left, right, or leveling up
 			else if(gameBoard->Move(player,move,size)){
 				gameBoard->nextBlock(player);
 				gameBoard->createNext(player);
@@ -64,11 +93,13 @@ try{
                         }
 				std::cout << *gameBoard << std::endl;
 		}
-		//next player's turn
+		// displays highscore
 		gameBoard->newHighscore();
+		// next player's turn
 		player = (player + 1)%2;
 		
 	}
+	// board catches the thrown losing player 
 }catch(int player){
 	std::cout << *gameBoard << std::endl;
 	if (player == 1){
@@ -82,8 +113,7 @@ try{
 	if (again == "yes"){
 		gameBoard->newHighscore();
 		gameBoard->Restart();
-		start();
-		
+		start();	
 	}
 
 goto endgame;
--- biquadris.h
+++ biquadris.h
@@ -15,13 +15,22 @@
 class Board;
 
 class Biquadris {
+	// an interpreter that takes in user input and interprets it to something actionable 
 	Interpreter inter;
+	// has a game board 
 	std::unique_ptr<Board> gameBoard;
+	// keeps track of the current player 
 	int player;
+	// inital start level
 	int startLevel=0;
+	// a stream source to take in commands
 	std::istream *source;
 
 public:
+	// Biquadris(int,string,string,int) initalizes a game where
+	// first int is the starting level
+	// strings are the files each player plays from
+	// seecond int the seed
 	Biquadris(int,std::string,std::string,int);
 	~Biquadris();
 	void start();
